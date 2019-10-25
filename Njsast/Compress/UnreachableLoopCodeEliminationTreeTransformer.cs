b8d1d3419c1aab4d68f16a563813ca5fd5d7b689
Miroslav Pokorný <miroslavpokorny94@gmail.com>
2019-10-25 14:37:36 +0200

    Refactored unreachable loop code elimination

--- Njsast/Compress/UnreachableLoopCodeEliminationTreeTransformer.cs
+++ Njsast/Compress/UnreachableLoopCodeEliminationTreeTransformer.cs
@@ -23,109 +23,48 @@ namespace Njsast.Compress
 
         protected override AstNode? Before(AstNode node, bool inList)
         {
-            if (_isProcessingSwitchStatement && node is AstCase astCase)
+            if (_isProcessingSwitchStatement)
             {
-                var safeLastCaseExpression = _lastCaseExpression;
-                var safeIsAfterLoopControl = _isAfterLoopControl;
-                _lastCaseExpression = astCase.Expression;
-                Descend();
-                _lastCaseExpression = safeLastCaseExpression;
-                _isAfterLoopControl = safeIsAfterLoopControl;
-                return astCase;
-            }
-
-            if (_isProcessingSwitchStatement && node is AstDefault)
-            {
-                var safeIsAfterLoopControl = _isAfterLoopControl;
-                Descend();
-                _isAfterLoopControl = safeIsAfterLoopControl;
-                return node;
+                switch (node)
+                {
+                    case AstCase astCase:
+                        return ProcessSwitchCaseNode(astCase);
+                    case AstDefault astDefault:
+                        return ProcessSwitchDefaultNode(astDefault);
+                }
             }
 
             if (node == _lastCaseExpression || node == _lastTryCatch?.Argname)
-            {
                 return node;
-            }
             
-            if (node is AstIterationStatement)
-            {
-                if (_isProcessingIterationStatement)
-                {
-                    return node;
-                }
+            
+            if (node is AstIterationStatement astIterationStatement)
+                return ProcessIterationStatement(astIterationStatement);
 
-                _isProcessingIterationStatement = true;
-                Descend();
-                _isProcessingIterationStatement = false;
-                return node;
-            }
-            
-            if (node == _lastIfAlternative)
-            {
+            if (node == _lastIfAlternative) 
                 _isAfterLoopControl = false;
-            }
 
             if (node == _lastTryCatch || node == _lastTryFinally)
-            {
-                var safeIsAfterLoopControl = _isAfterLoopControl;
-                _isAfterLoopControl = false;
-                Descend();
-                _isAfterLoopControl = safeIsAfterLoopControl;
-                return node;
-            }
+                return ProcessCatchOrFinally(node);
 
             if (_isAfterLoopControl)
-            {
                 return TryRemoveNode(node);
-            }
-            
-            if (node is AstIf astIf)
-            {
-                var safeLastIfAlternative = _lastIfAlternative;
-                _lastIfAlternative = astIf.Alternative;
-                Descend();
-                _lastIfAlternative = safeLastIfAlternative;
-                _isAfterLoopControl = false;
-                return astIf;
-            }
 
-            if (node is AstSwitch)
+            switch (node)
             {
-                var safeIsProcessingSwitch = _isProcessingSwitchStatement;
-                _isProcessingSwitchStatement = true;
-                Descend();
-                _isProcessingSwitchStatement = safeIsProcessingSwitch;
-                return node;
+                case AstIf astIf:
+                    return ProcessIfStatement(astIf);
+                case AstSwitch astSwitch:
+                    return ProcessSwitch(astSwitch);
+                case AstTry astTry:
+                    return ProcessTry(astTry);
+                case AstStatementWithBody astStatementWithBody:
+                    return ProcessStatementWithBody(astStatementWithBody);
+                case AstLoopControl astLoopControl:
+                    return ProcessLoopControl(astLoopControl);
+                default:
+                    return null;
             }
-
-            if (node is AstTry astTry)
-            {
-                var safeLastTryCatch = _lastTryCatch;
-                var safeLastTryFinally = _lastTryFinally;
-                _lastTryCatch = astTry.Bcatch;
-                _lastTryFinally = astTry.Bfinally;
-                 Descend();
-                _lastTryCatch = safeLastTryCatch;
-                _lastTryFinally = safeLastTryFinally;
-                return astTry;
-            }
-
-            if (node is AstStatementWithBody)
-            {
-                Descend();
-                _isAfterLoopControl = false;
-                return node;
-            }
-
-            if (node is AstLoopControl)
-            {
-                if (_isProcessingSwitchStatement && node is AstBreak)
-                    return node;
-                _isAfterLoopControl = true;
-                return node;
-            }
-
-            return null;
         }
 
         protected override AstNode? After(AstNode node, bool inList)
@@ -136,6 +75,93 @@ namespace Njsast.Compress
         protected override bool CanProcessNode(ICompressOptions options, AstNode node)
         {
             return options.EnableUnreachableCodeElimination && node is AstIterationStatement;
+        }
+
+        AstCase ProcessSwitchCaseNode(AstCase astCase)
+        {
+            var safeLastCaseExpression = _lastCaseExpression;
+            var safeIsAfterLoopControl = _isAfterLoopControl;
+            _lastCaseExpression = astCase.Expression;
+            Descend();
+            _lastCaseExpression = safeLastCaseExpression;
+            _isAfterLoopControl = safeIsAfterLoopControl;
+            return astCase;
+        }
+
+        AstDefault ProcessSwitchDefaultNode(AstDefault astDefault)
+        {
+            var safeIsAfterLoopControl = _isAfterLoopControl;
+            Descend();
+            _isAfterLoopControl = safeIsAfterLoopControl;
+            return astDefault;
+        }
+
+        AstIterationStatement ProcessIterationStatement(AstIterationStatement astIterationStatement)
+        {
+            if (_isProcessingIterationStatement)
+            {
+                return astIterationStatement;
+            }
+
+            _isProcessingIterationStatement = true;
+            Descend();
+            _isProcessingIterationStatement = false;
+            return astIterationStatement;
+        }
+
+        AstNode ProcessCatchOrFinally(AstNode catchOrFinallyNode)
+        {
+            var safeIsAfterLoopControl = _isAfterLoopControl;
+            _isAfterLoopControl = false;
+            Descend();
+            _isAfterLoopControl = safeIsAfterLoopControl;
+            return catchOrFinallyNode;
+        }
+
+        AstIf ProcessIfStatement(AstIf astIf)
+        {
+            var safeLastIfAlternative = _lastIfAlternative;
+            _lastIfAlternative = astIf.Alternative;
+            Descend();
+            _lastIfAlternative = safeLastIfAlternative;
+            _isAfterLoopControl = false;
+            return astIf;
+        }
+
+        AstSwitch ProcessSwitch(AstSwitch astSwitch)
+        {
+            var safeIsProcessingSwitch = _isProcessingSwitchStatement;
+            _isProcessingSwitchStatement = true;
+            Descend();
+            _isProcessingSwitchStatement = safeIsProcessingSwitch;
+            return astSwitch;
+        }
+
+        AstTry ProcessTry(AstTry astTry)
+        {
+            var safeLastTryCatch = _lastTryCatch;
+            var safeLastTryFinally = _lastTryFinally;
+            _lastTryCatch = astTry.Bcatch;
+            _lastTryFinally = astTry.Bfinally;
+            Descend();
+            _lastTryCatch = safeLastTryCatch;
+            _lastTryFinally = safeLastTryFinally;
+            return astTry;
+        }
+
+        AstStatementWithBody ProcessStatementWithBody(AstStatementWithBody astStatementWithBody)
+        {
+            Descend();
+            _isAfterLoopControl = false;
+            return astStatementWithBody;
+        }
+
+        AstLoopControl ProcessLoopControl(AstLoopControl astLoopControl)
+        {
+            if (_isProcessingSwitchStatement && astLoopControl is AstBreak)
+                return astLoopControl;
+            _isAfterLoopControl = true;
+            return astLoopControl;
         }
     }
 }
