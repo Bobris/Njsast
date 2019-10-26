c764fda66fbb499a507007a763db633473054281
Boris Letocha <boris.letocha@gmail.com>
2019-10-26 12:18:02 +0200

    Added Bundler tests and improved constant symbol detection.

---
+++ Test/Input/Bundler/Extend/out/cb-bundle.js
@@ -0,0 +1,22 @@
+!function(undefined) {
+    "use strict";
+    var __extendStatics, __extends, __assign, __values, __read, __await, DEBUG, Base, Derived, Main;
+    __extendStatics = Object.setPrototypeOf || {
+        __proto__: []
+    } instanceof Array && function(d, b) {
+        d.__proto__ = b;
+    } || function(d, b) {
+        var p;
+        for (p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    };
+    __extends;
+    __assign = Object.assign || function(t) {
+        var i, n, s, p;
+        for (i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+    };
+}();
+
---
+++ Test/Input/Bundler/Extend/out/cbm-bundle.js
@@ -0,0 +1,22 @@
+!function(t) {
+    "use strict";
+    var n, e, _, s, i, r, a, o, p, f;
+    n = Object.setPrototypeOf || {
+        __proto__: []
+    } instanceof Array && function(t, n) {
+        t.__proto__ = n;
+    } || function(t, n) {
+        var e;
+        for (e in n) if (n.hasOwnProperty(e)) t[e] = n[e];
+    };
+    e;
+    _ = Object.assign || function(t) {
+        var n, e, _, s;
+        for (n = 1, e = arguments.length; n < e; n++) {
+            _ = arguments[n];
+            for (s in _) if (Object.prototype.hasOwnProperty.call(_, s)) t[s] = _[s];
+        }
+        return t;
+    };
+}();
+
