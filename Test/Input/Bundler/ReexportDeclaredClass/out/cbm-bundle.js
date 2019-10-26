006e0870eec8744c41754c70da696470814889df
Boris Letocha <boris.letocha@gmail.com>
2019-10-26 18:00:24 +0200

    More bundler tests.

---
+++ Test/Input/Bundler/ReexportDeclaredClass/out/cbm-bundle.js
@@ -0,0 +1,29 @@
+!function(_) {
+    "use strict";
+    var t, s, e, n, r, a, i, o;
+    t = Object.setPrototypeOf || {
+        __proto__: []
+    } instanceof Array && function(_, t) {
+        _.__proto__ = t;
+    } || function(_, t) {
+        var s;
+        for (s in t) if (t.hasOwnProperty(s)) _[s] = t[s];
+    };
+    s = Object.assign || function(_) {
+        var t, s, e, n;
+        for (t = 1, s = arguments.length; t < s; t++) {
+            e = arguments[t];
+            for (n in e) if (Object.prototype.hasOwnProperty.call(e, n)) _[n] = e[n];
+        }
+        return _;
+    };
+    e;
+    n;
+    r;
+    a = !1;
+    i = _;
+    o = i;
+    function p() {}
+    p();
+}();
+
---
+++ Test/Input/Bundler/SimpleConst/out/cb-bundle.js
@@ -0,0 +1,27 @@
+!function(undefined) {
+    "use strict";
+    var __extendStatics, __assign, __values, __read, __await, DEBUG, __export_a;
+    __extendStatics = Object.setPrototypeOf || {
+        __proto__: []
+    } instanceof Array && function(d, b) {
+        d.__proto__ = b;
+    } || function(d, b) {
+        var p;
+        for (p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    };
+    __assign = Object.assign || function(t) {
+        var i, n, s, p;
+        for (i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+    };
+    __values;
+    __read;
+    __await;
+    DEBUG = !1;
+    __export_a = 42;
+    console.log(__export_a);
+}();
+
