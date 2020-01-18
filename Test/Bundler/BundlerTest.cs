0116423c1d7b407e1b2e2f32cabafde57814f1fe
Boris Letocha <boris.letocha@gmail.com>
2020-01-18 23:20:19 +0100

    Backport changes from bobril-build.

--- Test/Bundler/BundlerTest.cs
+++ Test/Bundler/BundlerTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Njsast.Ast;
 using Njsast.Bundler;
 using Njsast.Compress;
 using Njsast.Output;
@@ -120,6 +121,10 @@ namespace Test.Bundler
             }
 
             public void ReportTime(string name, TimeSpan duration)
+            {
+            }
+
+            public void ModifyBundle(string name, AstToplevel topLevelAst)
             {
             }
         }
