7f087cebb1753998245c93b50e84bf00c75beb85
Boris Letocha <boris.letocha@gmail.com>
2020-05-17 00:52:41 +0200

    Detect __exportStar with 2 parameters from TS 3.9.2

---
+++ Test/Input/Bundler/ExportAll2/out/cb-bundle.js
@@ -0,0 +1,8 @@
+(function(undefined) {
+    "use strict";
+    function fn(a, b) {
+        return a + b;
+    }
+    console.log(fn(1, 2));
+}).call(this);
+
---
+++ Test/Input/Bundler/ExportAll2/out/cbm-bundle.js
@@ -0,0 +1,8 @@
+(function(n) {
+    "use strict";
+    function t(n, t) {
+        return n + t;
+    }
+    console.log(t(1, 2));
+}).call(this);
+
