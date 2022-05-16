402a92ec43271ad51e00d8de322ad4bf53dd068d
Boris Letocha <boris.letocha@gmail.com>
2022-05-17 01:01:32 +0200

    External Imports First test

---
+++ Test/Input/Bundler/LibraryExternal/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getDescriptor = void 0;
+// @ts-ignore
+var ex = require("External");
+function getDescriptor() {
+    return { asd: ex.value };
+}
+exports.getDescriptor = getDescriptor;
