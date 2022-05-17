75369d682d1c4f8a8c3f804bc501000115d54d44
Boris Letocha <boris.letocha@gmail.com>
2022-05-18 00:57:00 +0200

    Fix compressor for ``()=>variable` code (variable wrongly marked as write usage).

--- Test/Input/Bundler/LibrarySimple/index.js
+++ Test/Input/Bundler/LibrarySimple/index.js
@@ -1,6 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getDescriptor = void 0;
+exports.getDescriptor = exports.setGlobals = exports.getGlobals = void 0;
+const getGlobals = () => globals;
+exports.getGlobals = getGlobals;
+const setGlobals = (a) => {
+    globals = {
+        isSortingDisabled: a,
+    };
+};
+exports.setGlobals = setGlobals;
+let globals = {
+    isSortingDisabled: false,
+};
 function getDescriptor() {
     return { asd: "df" };
 }
