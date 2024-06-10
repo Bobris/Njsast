30563e99f3eec8693367cd036d8ea85050e43f10
Boris Letocha <boris.letocha@gmail.com>
2024-06-10 14:47:17 +0200

    Support export enums from Library.

---
+++ Test/Input/Bundler/LibraryEnum/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LibraryEnum = void 0;
+var LibraryEnum;
+(function (LibraryEnum) {
+    LibraryEnum["Yes"] = "Yes";
+    LibraryEnum["No"] = "No";
+})(LibraryEnum || (exports.LibraryEnum = LibraryEnum = {}));
