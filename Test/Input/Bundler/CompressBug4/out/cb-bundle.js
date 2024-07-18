a7520959fc857e73ffd0a7a720f4cbea888143a1
Boris Letocha <boris.letocha@gmail.com>
2024-07-18 16:10:00 +0200

    Fix compression bug with if and try.

---
+++ Test/Input/Bundler/CompressBug4/out/cb-bundle.js
@@ -0,0 +1,15 @@
+(() => {
+    function x() {
+        if (this.prop) {
+            try {
+                something();
+            } catch (error) {
+                addError(error);
+            } finally {
+                log("finally");
+            }
+        }
+    }
+    x();
+})();
+
